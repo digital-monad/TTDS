54ccf2b5b62c7e8d97cfb47363427198d293a2ae
JHoweWowe <howejust@gmail.com>
2022-02-10 15:40:42 +0000

    Update generate lyrics script

--- data_collection/test_data/json/generate_artist_json_files.py
+++ data_collection/test_data/json/generate_artist_json_files.py
@@ -15,7 +15,7 @@ genius = lg.Genius(temporary_client_access_token,
                    skip_non_songs=True, 
                    excluded_terms=["(Remix)", "(Live)"], 
                    remove_section_headers=True,
-                   timeout=60,
+                   timeout=600,
                    retries=10)
 
 current_path = os.path.dirname(__file__)
@@ -23,12 +23,22 @@ file = os.path.normpath(current_path + os.sep + os.pardir + os.sep + 'albums_tex
 
 artist_list = list()
 
-# TBC: Read multiple files
+# Used for now
 with open(file, 'r', encoding='utf-8') as file:
     for line in file:
         artist = line.strip()
         artist_data = genius.search_artist(artist, include_features=True) # TODO: CHANGE THE MAX_SONGS
-        
-        artist_list.append(artist_data)
 
-genius.save_artists(artists= artist_list, overwrite=True)
+        artist_data.save_lyrics()
+
+
+# TBC: Used later when problems fixed - Read multiple files
+def save_multiple_artists():
+    with open(file, 'r', encoding='utf-8') as file:
+        for line in file:
+            artist = line.strip()
+            artist_data = genius.search_artist(artist, include_features=True) # TODO: CHANGE THE MAX_SONGS
+            
+            artist_list.append(artist_data)
+
+    genius.save_artists(artists= artist_list, overwrite=True)
