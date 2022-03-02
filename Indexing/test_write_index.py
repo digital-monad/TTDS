538d613e91b38b9bf1bdf47c7dc104dcc841becd
JHoweWowe <howejust@gmail.com>
2022-03-02 17:00:25 +0000

    Update write index format

--- Indexing/test_write_index.py
+++ Indexing/test_write_index.py
@@ -1,7 +1,8 @@
 # This script will ultimately write index pickle file into MongoDB
 
 # This inserts CSV file for SongsMetaData
-from bson.binary import Binary
+from operator import index
+from bson.objectid import ObjectId
 import configparser, json, os, pickle, pymongo
 from pymongo import *
 
@@ -14,15 +15,24 @@ password = config.get('mongodb', 'password')
 uri = base_uri + username + ':' + password + '@ttds-group-37.0n876.mongodb.net/ttds?retryWrites=true&w=majority'
 
 client = pymongo.MongoClient(uri)
-songs_meta_data_collection = client.ttds.invertedIndex
+inverted_index_collection = client.ttds.invertedIndex
 
 current_path = os.path.dirname(__file__)
 file = current_path + os.sep + 'test_index.pickle' # TODO: Iterate through relevant pickle files for index
 
 with open(file, 'rb') as input_file:
-    dictionary = pickle.load(input_file)
+    index_dictionary = pickle.load(input_file)
 
-my_list = [dictionary]
+for key_term in index_dictionary:
+    song_id = index_dictionary[key_term]
+    inverted_index_collection.insert_many([
+        {
+            "key": str(key_term),
+            key_term: song_id
+        }
+    ])
 
-print(my_list)
-songs_meta_data_collection.insert_many(my_list)
+inverted_index_collection.create_index('_id')
+inverted_index_collection.create_index('key')
+
+meow = list(inverted_index_collection.find({"key": {"$in": ['intro', 'look']}}))
