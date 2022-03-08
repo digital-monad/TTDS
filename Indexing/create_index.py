29de27a4f79207d65d63c613bc44fe0e1f5805c9
rohan-nittur <rohan.nittur@gmail.com>
2022-03-08 13:36:47 +0000

    Batch indexing

--- Indexing/create_index.py
+++ Indexing/create_index.py
@@ -17,22 +17,29 @@ index = {
 
 class Indexer:
 
-    def __init__(self, csv_file, stopping = False, line_id = 0):
+    def __init__(self, csv_file, batch_size, stopping = False, line_id = 0):
         self.index = {}
         self.song_metadata = {}
         self.line_metadata = {}
         self.artist = csv_file.split("/")[-1][:-4]
         self.stopping = stopping
+        self.batch_size = batch_size
         self.current_line_id = 0
         self.lyrics = pd.read_csv(csv_file)
 
-    def pickle(self):
-        with open(self.artist + "_index.pickle", 'wb') as handle:
+    def pickle(self, file_name):
+        with open(file_name + "_index.pickle", 'wb') as handle:
             pickle.dump(self.index, handle)
-        with open(self.artist + "_song_metadata.pickle", 'wb') as handle:
+        with open(file_name + "_song_metadata.pickle", 'wb') as handle:
             pickle.dump(self.song_metadata, handle)
-        with open(self.artist + "_line_metadata.pickle", 'wb') as handle:
+        with open(file_name + "_line_metadata.pickle", 'wb') as handle:
             pickle.dump(self.line_metadata, handle)
+        # Clear memory
+        self.index = {}
+        self.song_metadata = {}
+        self.line_metadata = {}
+
+
 
     def processSong(self, song_id, artist, title, album, year, date, lyrics):
         print(f"Processing song {title}")
@@ -66,12 +73,16 @@ class Indexer:
             self.current_line_id += 1
 
     def indexFile(self):
-        for song in zip(*[self.lyrics[i] for i in self.lyrics.columns]):
-            self.processSong(*song)
+        cols = zip(*[self.lyrics[i] for i in self.lyrics.columns])
+        for song in range(len(cols)):
+            self.processSong(*cols[song])
+            if song % self.batch_size == 0:
+                self.pickle(f"phatboi{song // self.batch_size}")
+        self.pickle("finalboi")
 
 parser = argparse.ArgumentParser(description='Convert CSV files to term positional inverted index.')
 parser.add_argument('--file', type=str, required=True, help='File path to CSV file for parsing')
+parser.add_argument('--batch-size', type=int, default=10000, help='Size of each pickle file')
 args = parser.parse_args()
-indexer = Indexer(args.file)
+indexer = Indexer(args.file, args.batch_size)
 indexer.indexFile()
-indexer.pickle()
