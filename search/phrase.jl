3ed5ed7fc7311e52e1b670b45aaac0c58884a7ec
rohan-nittur <rohan.nittur@gmail.com>
2022-03-13 21:47:49 +0000

    Phrase Search Julia

--- search/phrase.jl
+++ search/phrase.jl
@@ -1,3 +1,15 @@
+using PyCall
+
+py"""
+import pickle
+def load_pickle(file_name):
+    with open(file_name + '.pickle', 'rb') as f:
+        return pickle.load(f)
+"""
+
+load_pickle = py"load_pickle"
+
+
 """
     phraseSearch(phrase::Vector{String}, index::Dict, song::Bool)
 
@@ -12,20 +24,20 @@ julia> phraseSearch(["Teenage", "wasteland"], index, false)
 """
 function phraseSearch(phrase::Vector{String}, index::Dict, song::Bool)
     matchingDocs = Set{Int}() # Set of all doc ids matching the query
-    sequenceMap = Dict() # Dictionary mapping successive terms in the phrase to their view in the index
+    sequenceMap = Dict{Int, Dict{Int,Vector{Int}}}() # Dictionary mapping successive terms in the phrase to their view in the index
 
     if song
         # Song level phrase search
         for i in 1:length(phrase)
-            posting = Dict(song_id => reduce(vcat,values(lines)) for (song_id, lines) in index[phrase[i]])
-            sequenceMap[i] = i == 1 ? posting : Dict(song_id => listing for (song_id, listing) in posting if song_id ∈ sequenceMap[i-1])
+            posting = Dict{Int, Vector{Int}}(song_id => reduce(vcat,values(lines)) for (song_id, lines) in index[phrase[i]])
+            sequenceMap[i] = i == 1 ? posting : Dict{Int,Vector{Int}}(song_id => listing for (song_id, listing) in posting if song_id ∈ keys(sequenceMap[i-1]))
         end
-        matrixCount = Dict()
+        matrixCount = Dict{Int,Int}()
         for song_id in keys(sequenceMap[length(phrase)])
             matching = false
             for i in 1:length(phrase)
                 for position in sequenceMap[i][song_id]
-                    updatedValue = get(matrixCount, position - i - 1, 0) + 1
+                    updatedValue = get(matrixCount, position - i, 0) + 1
                     if updatedValue == length(phrase)
                         push!(matchingDocs, song_id)
                         matching = true
@@ -38,25 +50,33 @@ function phraseSearch(phrase::Vector{String}, index::Dict, song::Bool)
         end
     else
         for i in 1:length(phrase)
-            posting = Dict(line_id => positions for song in values(index[phrase[i]]) for (line_id, positions) in song)
-            sequenceMap[i] = i == 1 ? posting : Dict(line_id => listing for (line_id, listing) in posting if line_id ∈ sequenceMap[i-1])
+            posting = Dict{Int, Vector{Int}}(line_id => positions for song in values(index[phrase[i]]) for (line_id, positions) in song)
+            sequenceMap[i] = i == 1 ? posting : Dict{Int,Vector{Int}}(line_id => listing for (line_id, listing) in posting if line_id ∈ keys(sequenceMap[i-1]))
         end
-        matrixCount = Dict()
-        for line_id in keys(sequenceMap[length(phrase)])
-            matching = false
-            for i in 1:length(phrase)
-                for position in sequenceMap[i][line_id]
-                    updatedValue = get(matrixCount, position - i - 1, 0) + 1
-                    if updatedValue == length(phrase)
-                        push!(matchingDocs, line_id)
-                        matching = true
-                        break
-                    end
-                    matrixCount[position - i] = updatedValue
-                end
-                matching && break
-            end
-        end
+        # matrixCount = Dict{Int,Int}()
+        # for line_id in keys(sequenceMap[length(phrase)]) # Go through each line for which all terms appear
+        #     matching = false
+        #     for i in 1:length(phrase) # For every term in the phrase
+        #         for position in sequenceMap[i][line_id]
+        #             updatedValue = get(matrixCount, position - i, 0) + 1
+        #             if updatedValue == length(phrase)
+        #                 push!(matchingDocs, line_id)
+        #                 matching = true
+        #                 break
+        #             end
+        #             matrixCount[position - i] = updatedValue
+        #         end
+        #         matching && break
+        #     end
+        # end
     end
     matchingDocs
 end
+
+function main()
+    index = load_pickle("search/Test_Lyrics_Eminem_index")
+    index = convert(Dict{String, Dict{Int,Dict{Int,Vector{Int}}}}, index)
+    # @elapsed phraseSearch(["you"], index, false)
+    values(index["induct"])
+end
+main()
